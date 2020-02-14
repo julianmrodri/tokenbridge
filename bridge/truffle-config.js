fec603a2665acdb0cc34fc1d4a9a853c6ea81f69
pedro <pedro@rsk.co>
2020-02-14 16:12:18 -0300

    Upgrade Bridge and Side Token to be ERC677 compatible Some tokens like LINK are ERC677 and they need that interface to work, if we wanted to move the chain link to RSK and use the crossed token we need the SideToken to implement that interface. Also increased the solidity version to 0.5.16

--- bridge/truffle-config.js
+++ bridge/truffle-config.js
@@ -93,7 +93,7 @@ module.exports = {
   },
   compilers: {
       solc: {
-        version: "0.5.15",
+        version: "0.5.16",
         settings: {
           optimizer: {
             enabled: false,
